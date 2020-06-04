3d5e3c4aee4cfc7f84039aa83f3a13009149380c
Korey Brooks <korey.brooks@mail.com>
2020-06-04 08:28:27 -0500

    updated app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Counter from './Components/Counter';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+const INCREMENTS = [2, 3, 5, 10, 100];
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Counter App</h2>
-      </header>
-      <Counter/>
+      <div className="container-fluid">
+        <h1>Counter App</h1>
+        <div>Count: {count}</div>
+
+        <button
+          type="button"
+          className="btn btn-primary btn-lg btn-block"
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+
+        <button
+          type="button"
+          className="btn btn-secondary btn lg btn-block"
+          onClick={() => setCount(count - 1)}
+        >
+          -
+        </button>
+        <div>
+          <p>Increments Of </p>
+          {INCREMENTS.map((value) => {
+            return (
+              <button
+                className="btn btn-dark"
+                onClick={() => setCount(count + value)}
+              >
+                +{value}
+              </button>
+            );
+          })}
+        </div>
+        <button
+          className="btn btn-lg btn secondary"
+          onClick={() => setCount(0)}
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
